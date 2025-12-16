9ec34da399fc2c7c96f8d02afb618636a5aa95e4
CMohamed <chadad.mohamed@gmail.com>
2025-12-16 03:08:37 +0100

    Add an ip to allowed origin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  allowedDevOrigins: ["http://68.183.75.175:3000", "http://68.183.75.175", "http://localhost:3000"],
+  experimental: {
+    allowedDevOrigins: [
+      "http://68.183.75.175:3000",
+      "http://68.183.75.175",
+      "http://localhost:3000",
+    ],
+  },
 };
 
 export default nextConfig;
